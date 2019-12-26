56ba6fc047abc6d75359f4fcaa6224560b7d2143
srinagula <srinagula936@gmail.com>
2019-12-26 17:08:00 -0500

    added authentication service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,8 @@ export class LoginComponent implements OnInit {
   errorMessage = 'Invalid Credentials';
   invalidLogin = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, 
+    private: HardcodedAuthenticationService: HardcodedAuthenticationService) { }
 
   ngOnInit() {
   }
